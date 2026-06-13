c5560080dbf12c935a648d2c821ca1f1925ec7e1
Lokesh Mudhalvan <lokesh_mudhalvan@akane.waseda.jp>
2026-06-14 07:59:15 +0900

    format temperature to have upto 2 decimal places

--- src/transform.py
+++ src/transform.py
@@ -10,10 +10,12 @@ def categorize_weather(temp):
 
 
 def convert_to_farenheit(temp):
-    return (temp * 9 / 5) + 32
+    f = (temp * 9 / 5) + 32
+    return round(f, 2)
 
 
 def transform_weather_data(df):
+    df["temperatureInCelcius"] = df["temperatureInCelcius"].round(2)
     df.loc[:, "weatherAlert"] = df["temperatureInCelcius"].apply(categorize_weather)
     df.loc[:, "temperatureInFarenheit"] = df["temperatureInCelcius"].apply(
         convert_to_farenheit
